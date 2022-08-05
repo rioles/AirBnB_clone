b7e4e70ac71f4e1420a39e0c6ad47c7c798c2e99
rioles <rioles1992@gmail.com>
2022-08-05 15:25:54 +0000

    date format

--- models/base_model.py
+++ models/base_model.py
@@ -6,8 +6,9 @@ import uuid
 
 class BaseModel:
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         "Initialize the instance attributes"
+        tform = "%Y-%m-%dT%H:%M:%S.%f"
         self.id = str(uuid.uuid4())
         self.created_at = datetime.today()
         self.updated_at = datetime.now()
